6885a7d88fe1526f6cf68f7a8f8b31ccb54a7444
Jinyoung Han <jyhan35@gmail.com>
2021-03-24 15:20:08 +0900

    Add 공지사항 등록, 수정

--- backend/TomorrowBoot/src/main/java/com/Tomorrow/myapp/controller/NoticeController.java
+++ backend/TomorrowBoot/src/main/java/com/Tomorrow/myapp/controller/NoticeController.java
@@ -1,24 +1,12 @@
-package  com.Tomorrow.myapp.controller;
+package com.Tomorrow.myapp.controller;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,11 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.model.S3DataSource.Utils;
-import  com.Tomorrow.myapp.model.NoticeDto;
-import  com.Tomorrow.myapp.service.NoticeService;
+import com.Tomorrow.myapp.model.NoticeDto;
+import com.Tomorrow.myapp.service.NoticeService;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -41,78 +27,67 @@ import io.swagger.annotations.ApiOperation;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/notice")
 public class NoticeController {
-	
-	
-	private final NoticeService noticeService;
-	
-	@Autowired
-	public NoticeController(NoticeService noticeService) {
-		this.noticeService = noticeService;
-	}
-	
-	 @ApiOperation(value = "공지사항 등록", notes = "공지사항 등록", response = Map.class)
-	  @PostMapping(value = ("/writenotice"))
-	 public ResponseEntity<String> writeNotice(@RequestBody NoticeDto notice,HttpServletRequest req) throws IOException {
-		 String conclusion = "";
-		    HttpStatus status = HttpStatus.ACCEPTED;
-		    Map<String, Object> noticemap = new HashMap<String, Object>();
-		     if(noticeService.writeNotice(notice,noticemap)) {
-		    	conclusion = "SUCCESS";
-		    }
-		    else {
-		    	conclusion = "FAIL";
-		    }
-		    return new ResponseEntity<String>(conclusion, status);
-	 }
-	 @ApiOperation(value = "공지사항 목록", notes = "공지사항 목록", response = List.class)
-	  @GetMapping("/listnotice")
-	  public ResponseEntity<List<NoticeDto>> listnotice(HttpServletRequest req) throws SQLException {
-		   System.out.println(req);
-		    Map<String, Object> resultMap = new HashMap<>();
-		    HttpStatus status = HttpStatus.ACCEPTED;
-		    List<NoticeDto> list = new ArrayList<>();
-		    list = noticeService.listNotice();
-		    return new ResponseEntity<List<NoticeDto>>(list,status);
-	  }
-	  //공지사항 상세정보
-	  @ApiOperation(value = "공지사항 상세정보", notes = "공지사항 상세정보", response = Map.class)
-	  @GetMapping("/{no}")
-	  public ResponseEntity<NoticeDto> getNoticeInfo(@PathVariable(value="no") int noticeno, HttpServletRequest req) throws SQLException, IOException {
-		  NoticeDto noticetmp = noticeService.getNoticeInfo(noticeno);
-		    HttpStatus status = HttpStatus.ACCEPTED;
-		    return new ResponseEntity<NoticeDto>(noticetmp, HttpStatus.OK);
-	  }
-	  //공지사항정보수정
-	  @ApiOperation(value = "공지사항 수정", notes = "공지사항 수정", response = Map.class)
-	  @PutMapping("")
-	  public ResponseEntity<String> updateNotice(@RequestBody NoticeDto notice, HttpServletRequest req) throws SQLException, IOException {
-	    System.out.println(req);
-	    String conclusion = "SUCCESS";
-	    HttpStatus status = HttpStatus.ACCEPTED;
-	    System.out.println("put to /meeting done");
-	    System.out.println("미팅수정");
-	    System.out.println(notice.toString());
-	    Map<String, Object> noticemap = new HashMap<String, Object>();
-	    if(noticeService.update(notice,noticemap)) {
-	    	conclusion = "SUCCESS";
-	    }
-	    else {
-	    	conclusion = "FAIL";
-	    }
-	    return new ResponseEntity<String>(conclusion, status);
-	  }
-	  
-	  //공지사항삭제
-	  @ApiOperation(value = "공지사항삭제", notes = "공지사항삭제", response = Map.class)
-	  @DeleteMapping("/{no}")
-	  public ResponseEntity<Map<String, Object>> deletenotice(@PathVariable(value="no") int noticeno, HttpServletRequest req) {
-	    System.out.println(req);
-	    Map<String, Object> resultMap = new HashMap<>();
-	    HttpStatus status = HttpStatus.ACCEPTED;
-	    System.out.println("delete to /notice done");
-	    System.out.println("공지사항삭제");
-	    noticeService.delete(noticeno);
-	    return new ResponseEntity<Map<String, Object>>(resultMap, status);
-	  }
-	  
+    private final NoticeService noticeService;
+
+    @Autowired
+    public NoticeController(NoticeService noticeService) {
+        this.noticeService = noticeService;
+    }
+
+    /**
+	 *
+	 * @param noticeDto: 공지사항 정보
+	 *
+	 */
+    @ApiOperation(value = "공지사항 등록", notes = "공지사항 등록", response = Map.class)
+    @PostMapping(value = ("/"))
+    public ResponseEntity<String> writeNotice(@RequestBody NoticeDto noticeDto) throws SQLException {
+        String conclusion = "";
+        if (noticeService.writeNotice(noticeDto)) {
+            conclusion = "SUCCESS";
+        } else {
+            conclusion = "FAIL";
+        }
+        return new ResponseEntity<String>(conclusion, HttpStatus.ACCEPTED);
+    }
+
+    @ApiOperation(value = "공지사항 목록", notes = "공지사항 목록", response = List.class)
+    @GetMapping("/list")
+    public ResponseEntity<List<NoticeDto>> listNotice() throws SQLException {
+        return new ResponseEntity<>(noticeService.listNotice(), HttpStatus.ACCEPTED);
+    }
+
+    //공지사항 상세정보
+    @ApiOperation(value = "공지사항 상세정보", notes = "공지사항 상세정보", response = Map.class)
+    @GetMapping("/{no}")
+    public ResponseEntity<NoticeDto> getNoticeInfo(@PathVariable(value = "no") int noticeNo) throws SQLException, IOException {
+        return new ResponseEntity<NoticeDto>(noticeService.getNoticeInfo(noticeNo), HttpStatus.ACCEPTED);
+    }
+
+    //공지사항정보수정
+    @ApiOperation(value = "공지사항 수정", notes = "공지사항 수정", response = Map.class)
+    @PutMapping("")
+    public ResponseEntity<String> updateNotice(@RequestBody NoticeDto noticeDto) throws SQLException, IOException {
+        String conclusion = "";
+        if (noticeService.updateNotice(noticeDto)) {
+            conclusion = "SUCCESS";
+        } else {
+            conclusion = "FAIL";
+        }
+        return new ResponseEntity<String>(conclusion, HttpStatus.ACCEPTED);
+    }
+
+    //공지사항삭제
+    @ApiOperation(value = "공지사항삭제", notes = "공지사항삭제", response = Map.class)
+    @DeleteMapping("/{no}")
+    public ResponseEntity<String> deletenotice(@PathVariable(value = "no") int noticeNo) {
+        String conclusion = "";
+        if(noticeService.deleteNotice(noticeNo)){
+        	conclusion = "SUCCESS";
+		}else {
+        	conclusion = "FAIL";
+		}
+        return new ResponseEntity<>(conclusion, HttpStatus.ACCEPTED);
+    }
+
 }
--- backend/TomorrowBoot/src/main/java/com/Tomorrow/myapp/model/NoticeDto.java
+++ backend/TomorrowBoot/src/main/java/com/Tomorrow/myapp/model/NoticeDto.java
@@ -14,139 +14,113 @@ public class NoticeDto {
 	private String file1;
 	private String file2;
 	private String file3;
-	private int meetingno;
-	private boolean head;
-	
-	
-	
+
+
+	public NoticeDto(int no, String title, String detail, String date, String writer, int hit, String file1, String file2, String file3) {
+		this.no = no;
+		this.title = title;
+		this.detail = detail;
+		this.date = date;
+		this.writer = writer;
+		this.hit = hit;
+		this.file1 = file1;
+		this.file2 = file2;
+		this.file3 = file3;
+	}
+
 	public int getROWNUM() {
 		return ROWNUM;
 	}
-	public void setROWNUM(int rOWNUM) {
-		ROWNUM = rOWNUM;
+
+	public void setROWNUM(int rnum) {
+		ROWNUM = rnum;
 	}
-	public NoticeDto(int no, String title, String detail, String date, String writer, int hit, String file1,
-			String file2, String file3, int meetingno, boolean head) {
-		super();
-		this.no = no;
-		this.title = title;
-		this.detail = detail;
-		this.date = date;
-		this.writer = writer;
-		this.hit = hit;
-		this.file1 = file1;
-		this.file2 = file2;
-		this.file3 = file3;
-		this.meetingno = meetingno;
-		this.head = head;
-	}
-	public NoticeDto() {
-		super();
-	}
-	public NoticeDto(int no, String title, String detail, String date, String writer, int hit, String file1,
-			String file2, String file3) {
-		super();
-		this.no = no;
-		this.title = title;
-		this.detail = detail;
-		this.date = date;
-		this.writer = writer;
-		this.hit = hit;
-		this.file1 = file1;
-		this.file2 = file2;
-		this.file3 = file3;
-	}
-	
-	
-	
-	
-	public NoticeDto(int no, String title, String detail, String date, String writer, int hit, String file1,
-			String file2, String file3, int meetingno) {
-		super();
-		this.no = no;
-		this.title = title;
-		this.detail = detail;
-		this.date = date;
-		this.writer = writer;
-		this.hit = hit;
-		this.file1 = file1;
-		this.file2 = file2;
-		this.file3 = file3;
-		this.meetingno = meetingno;
-	}
+
 	public int getNo() {
 		return no;
 	}
+
 	public void setNo(int no) {
 		this.no = no;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDetail() {
 		return detail;
 	}
+
 	public void setDetail(String detail) {
 		this.detail = detail;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public String getWriter() {
 		return writer;
 	}
+
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
+
 	public int getHit() {
 		return hit;
 	}
+
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
+
 	public String getFile1() {
 		return file1;
 	}
+
 	public void setFile1(String file1) {
 		this.file1 = file1;
 	}
+
 	public String getFile2() {
 		return file2;
 	}
+
 	public void setFile2(String file2) {
 		this.file2 = file2;
 	}
+
 	public String getFile3() {
 		return file3;
 	}
+
 	public void setFile3(String file3) {
 		this.file3 = file3;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "NoticeDto [ROWNUM=" + ROWNUM + ", no=" + no + ", title=" + title + ", detail=" + detail + ", date="
-				+ date + ", writer=" + writer + ", hit=" + hit + ", file1=" + file1 + ", file2=" + file2 + ", file3="
-				+ file3 + ", meetingno=" + meetingno + ", head=" + head + "]";
+		return "NoticeDto{" +
+				"ROWNUM=" + ROWNUM +
+				", no=" + no +
+				", title='" + title + '\'' +
+				", detail='" + detail + '\'' +
+				", date='" + date + '\'' +
+				", writer='" + writer + '\'' +
+				", hit=" + hit +
+				", file1='" + file1 + '\'' +
+				", file2='" + file2 + '\'' +
+				", file3='" + file3 + '\'' +
+				'}';
 	}
-	public int getMeetingno() {
-		return meetingno;
-	}
-	public void setMeetingno(int meetingno) {
-		this.meetingno = meetingno;
-	}
-	public boolean isHead() {
-		return head;
-	}
-	public void setHead(boolean head) {
-		this.head = head;
-	}
-
-	
 }
