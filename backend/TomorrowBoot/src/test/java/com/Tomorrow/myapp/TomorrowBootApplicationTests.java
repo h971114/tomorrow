3ef17e6c38b6dfd2a91f7040d550cfee12fc896a
sujin <h971114@naver.com>
2021-03-31 14:00:51 +0900

    modify

---
+++ backend/TomorrowBoot/src/test/java/com/Tomorrow/myapp/TomorrowBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tomorrow.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
