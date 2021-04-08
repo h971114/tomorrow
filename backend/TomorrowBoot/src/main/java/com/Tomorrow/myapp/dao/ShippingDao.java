49ee18998c0cb91f23ba977ed45f66e91edf709b
Jinyoung Han <jyhan35@gmail.com>
2021-04-08 14:49:21 +0900

    Update 배송상태 가져오기, 배송상태 변경, menu_id 추가

--- backend/TomorrowBoot/src/main/java/com/Tomorrow/myapp/dao/ShippingDao.java
+++ backend/TomorrowBoot/src/main/java/com/Tomorrow/myapp/dao/ShippingDao.java
@@ -2,6 +2,10 @@ package com.Tomorrow.myapp.dao;
 
 import com.Tomorrow.myapp.model.ShippingDto;
 
+import java.util.List;
+
 public interface ShippingDao {
 	public void insertInfo(ShippingDto shipping);
+	public void updateStatus(ShippingDto shippingDto);
+	public List getShippingDto(ShippingDto shippingDto);
 }
